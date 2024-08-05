94cd89d58e79126007cf0bed19b6b7db3ffbea71
Pablo Lira <61480904+pablowilliam@users.noreply.github.com>
2024-08-05 23:04:04 +0100

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -22,16 +22,8 @@ print_banner() {
   printf "\n\n"
 
 printf "${CYAN_LIGHT}";
-
-
-printf ${CYAN_LIGHT}" ____   ____  ________  ____    ____   ________    _       ________  ________  _______   \n";  
-printf ${CYAN_LIGHT}"|_  _| |_  _||_   __  ||_   \  /   _| |_   __  |  / \     |  __   _||_   __  ||_   __ \  \n"; 
-printf ${CYAN_LIGHT}"  \ \   / /    | |_ \_|  |   \/   |     | |_ \_| / _ \    |_/  / /    | |_ \_|  | |__) | \n";
-printf ${CYAN_LIGHT}"   \ \ / /     |  _| _   | |\  /| |     |  _|   / ___ \      .'.' _   |  _| _   |  __ /  \n";
-printf ${CYAN_LIGHT}"    \ ' /     _| |__/ | _| |_\/_| |_   _| |_  _/ /   \ \_  _/ /__/ | _| |__/ | _| |  \ \_\n";
-printf ${CYAN_LIGHT}"     \_/     |________||_____||_____| |_____||____| |____||________||________||____| |___|\n";
                                                                                                                                                          
-  printf "            \033[1;33m        © CANAL VEM FAZER - https://www.youtube.com/channel/UCwFO9ylM7gHxYIXfJqAo1vQ\n";
+  printf "            \033[1;33m        © MultiChannel - https://site-multichannel.pt";
   printf "${NC}";
 
   printf "\n"
